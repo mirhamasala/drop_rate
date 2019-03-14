a0d69bdde06cbb5069426bf3fb2a8b7c5c8fb1d0
Mirha Masala <mirha.masala@me.com>
2019-03-14 21:42:09 +0800

    Use API to populate array, add dataset

--- main.js
+++ main.js
@@ -21,42 +21,36 @@
 //     "spinach"
 // ];
 
-
 const ketoFoods = [];
 
 // function getResults() {
-    const url = 'http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7';
-    fetch(url)
-        .then(response => response.json())
-        .then(data => {
-            data.Search.forEach(result => {
-                ketoFoods.push(result.Title);
-            })
-        })
-        .catch(function(error) {
+const url = 'http://www.omdbapi.com/?s=harry potter&apikey=adf1f2d7';
+fetch(url)
+    .then(response => response.json())
+    .then(data => ketoFoods.push(...data.Search))
+    .catch(function(error) {
         console.log(error);
     })
 // }
 
-function findMatches(foodToMatch, ketoFoods) {
-    const regex = new RegExp(foodToMatch, 'gi');
-    return ketoFoods.filter(food => {
-        return food.match(regex);
-    })
-}
+// function findMatches(foodToMatch, ketoFoods) {
+//     const regex = new RegExp(foodToMatch, 'gi');
+//     return ketoFoods.filter(food => {
+//         return food.match(regex);
+//     })
+// }
 
 function displayMatches(e) {
     if(e.target.value === "") {
         hideMatches();
     } else {
         suggestions.classList.remove("hide");
-        const matches = findMatches(this.value, ketoFoods);
-        const html = matches.map(food => {
+        const html = ketoFoods.map(food => {
             const regex = new RegExp(this.value, 'gi');
-            const foodName = food.replace(regex, `<span class="highlight">${this.value}</span>`);
-            return `
+            const foodName = food.Title.replace(regex, `<span data-image="${food.Poster}" class="highlight">${this.value}</span>`);
+            return (`
             <li><a href="#">${foodName}</a></li>
-           `;
+            `);
         }).join('');
         suggestions.innerHTML = html;
     }
@@ -68,6 +62,7 @@ function hideMatches() {
 
 const searchInput = document.querySelector('.search');
 const suggestions = document.querySelector('.suggestions');
+const images = document.querySelector('.images');
 
 searchInput.addEventListener("keyup", displayMatches);
 searchInput.addEventListener("focusin", displayMatches);
